4f86bf9f18571f8e19d0ee03603ee08a0b4817b3
zhangqingya <1351073251@qq.com>
2024-09-21 00:39:49 +0800

    完成作业

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include ".assignment/test.h"
-
+void swap(int &x,int &y)
+{
+    int t=x;
+    x=y;
+    y=t;
+}
 // 比较三个数的大小
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
-    // 在这里完成你的代码
+    if(a > b)
+    {
+        swap(a, b);
+
+        if(b > c)
+        {
+            swap(b, c);
+        }
+
+        if(a > b)
+        {
+            swap(a, b);
+        }
+    }
+    result.min = a;
+    result.mid = b;
+    result.max = c;// 在这里完成你的代码
 }
